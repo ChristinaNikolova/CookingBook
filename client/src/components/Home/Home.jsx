39bd15b521183a484c5ac6a817d5bd391e580d5b
christinanikolova <christinanikolova@github.com>
2025-11-26 17:18:11 +0200

    add link to the home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <section id={styles.home}>
       <h1 className={styles["home-title"]}>My CookingBook</h1>
-      <button className={styles["home-btn"]} type="button">
+      <Link to="/recipe/create" className={styles["home-btn"]}>
         Добави рецепта
-      </button>
+      </Link>
     </section>
   );
 }
