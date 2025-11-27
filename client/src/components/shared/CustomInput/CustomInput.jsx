1bad5c5dba7f9f5e2bf942f5ed23b6eceed8bbe5
christinanikolova <christinanikolova@github.com>
2025-11-27 17:23:34 +0200

    extend CustomInput component

--- client/src/components/shared/CustomInput/CustomInput.jsx
+++ client/src/components/shared/CustomInput/CustomInput.jsx
@@ -1,10 +1,35 @@
 import styles from "./CustomInput.module.css";
 
-export default function CustomInput({ label, type = "text" }) {
+export default function CustomInput({
+  tag = "input",
+  label,
+  type = "text",
+  name,
+  value,
+  onChange,
+  onBlur,
+}) {
   return (
     <div className={styles["input-wrapper"]}>
       <label htmlFor="">{label}</label>
-      <input type={type} />
+      {tag === "input" ? (
+        <input
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      ) : (
+        <textarea
+          name={name}
+          id=""
+          rows={12}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        ></textarea>
+      )}
     </div>
   );
 }
