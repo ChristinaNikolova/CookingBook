d2c679ec362ecd41fff30e5242fdb931d1c72b75
christinanikolova <christinanikolova@github.com>
2025-11-27 06:25:40 +0200

    reorder imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header/Header";
-import styles from "./App.module.css";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./NotFound/NotFound";
+import styles from "./App.module.css";
 
 function App() {
   const { pathname } = useLocation();
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import styles from "./Home.module.css";
 import Button from "../shared/Button/Button";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
