5e8773d7fbdd26fa45eaa3714aafc26546646825
christinanikolova <christinanikolova@github.com>
2025-12-01 12:14:46 +0200

    fix path for the RecipeItem

--- client/src/components/Recipe/RecipeItem/RecipeItem.jsx
+++ client/src/components/Recipe/RecipeItem/RecipeItem.jsx
@@ -1,12 +1,12 @@
 import ButtonLink from "../../shared/ButtonLink/ButtonLink";
 import styles from "./RecipeItem.module.css";
 
-export default function RecipeItem({ id, title, img }) {
+export default function RecipeItem({ id, categoryId, title, img }) {
   return (
     <li className={styles["recipes-item"]}>
       <img className={styles["recipes-item-img"]} src={img} alt={title} />
       <h4 className={styles["recipes-item-title"]}>{title}</h4>
-      <ButtonLink path={`/recipe/${id}`} text="Прочети" />
+      <ButtonLink path={`/recipe/${categoryId}/${id}`} text="Прочети" />
     </li>
   );
 }
