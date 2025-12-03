b171f102de9e9ee4e30910ab1526520343768758
christinanikolova <christinanikolova@github.com>
2025-12-03 13:24:49 +0200

    extract NoteItem

--- client/src/components/Note/All/All.jsx
+++ client/src/components/Note/All/All.jsx
@@ -3,6 +3,7 @@
 // todo use Memo
 
 import CreateNote from "../Create/Create";
+import NoteItem from "../NoteItem/NoteItem";
 import styles from "./All.module.css";
 
 export default function AllNotes() {
@@ -11,31 +12,10 @@ export default function AllNotes() {
       <h2 className={styles["notes-title"]}>Моите бележки</h2>
       <CreateNote />
       <ul className={styles["notes-list"]}>
-        <li className={styles["notes-item"]}>
-          <i className="fa-regular fa-note-sticky"></i>
-          <span>Бележка 5</span>
-          <i className="fa-solid fa-xmark" title="Изтрий"></i>
-        </li>
-        <li className={styles["notes-item"]}>
-          <i className="fa-regular fa-note-sticky"></i>
-          <span>Бележка 5</span>
-          <i className="fa-solid fa-xmark" title="Изтрий"></i>
-        </li>
-        <li className={styles["notes-item"]}>
-          <i className="fa-regular fa-note-sticky"></i>
-          <span>Бележка 5</span>
-          <i className="fa-solid fa-xmark" title="Изтрий"></i>
-        </li>
-        <li className={styles["notes-item"]}>
-          <i className="fa-regular fa-note-sticky"></i>
-          <span>Бележка 5</span>
-          <i className="fa-solid fa-xmark" title="Изтрий"></i>
-        </li>
-        <li className={styles["notes-item"]}>
-          <i className="fa-regular fa-note-sticky"></i>
-          <span>Бележка 5</span>
-          <i className="fa-solid fa-xmark" title="Изтрий"></i>
-        </li>
+        <NoteItem description="Бележка 1" />
+        <NoteItem description="Бележка 2" />
+        <NoteItem description="Бележка 3" />
+        <NoteItem description="Бележка 4" />
       </ul>
     </section>
   );
---
+++ client/src/components/Note/NoteItem/NoteItem.jsx
@@ -0,0 +1,11 @@
+import styles from "./NoteItem.module.css";
+
+export default function NoteItem({ description }) {
+  return (
+    <li className={styles["note-item"]}>
+      <i className="fa-regular fa-note-sticky"></i>
+      <span>{description}</span>
+      <i className="fa-solid fa-xmark" title="Изтрий"></i>
+    </li>
+  );
+}
