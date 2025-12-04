157af073291b0e2675538d4d76ef67e36de86853
christinanikolova <christinanikolova@github.com>
2025-12-04 07:13:57 +0200

    add formHelpers to prepare the data for the server

---
+++ client/src/utils/formHelpers.js
@@ -0,0 +1,29 @@
+export const formHelpers = {
+  prepareData: (values, files) => {
+    const trimmedValues = formHelpers.trimValues(values);
+    const hasFiles = Object.keys(files).some((key) => files[key]);
+
+    if (hasFiles) {
+      const formData = new FormData();
+
+      Object.keys(trimmedValues).forEach((key) => {
+        formData.append(key, files[key] || trimmedValues[key]);
+      });
+
+      return formData;
+    }
+
+    return trimmedValues;
+  },
+
+  trimValues: (values) => {
+    return Object.keys(values).reduce((acc, curr) => {
+      acc[curr] = formHelpers.trimValue(values[curr]);
+      return acc;
+    }, {});
+  },
+
+  trimValue: (value) => {
+    return typeof value === "string" ? value.trim() : value;
+  },
+};
