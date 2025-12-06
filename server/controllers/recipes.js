df7bc13fb9b054ddb7ee30432c9f432d64358c40
christinanikolova <christinanikolova@github.com>
2025-12-06 06:03:16 +0200

    add delete recipe

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const upload = require("../middlewares/fileUpload");
 const { hasUser } = require("../middlewares/guards");
-const { create, getById, getByCategory } = require("../services/recipes");
+const {
+  create,
+  getById,
+  getByCategory,
+  deleteById,
+} = require("../services/recipes");
 const { filePaths } = require("../utils/constants/global");
 const { mapErrors } = require("../utils/parser");
 
-// sled submit, scroll to the top to see the rroro
+// todo fix server error css
 router.post("/", hasUser(), upload.single("image"), async (req, res) => {
   try {
     const imagePath = filePaths.RECIPES + req.file.filename;
@@ -48,6 +53,17 @@ router.get("/:id", hasUser(), async (req, res) => {
   try {
     const id = req.params.id;
     const recipe = await getById(id);
+    res.json(recipe);
+  } catch (error) {
+    const message = mapErrors(error);
+    res.status(400).json({ message });
+  }
+});
+
+router.delete("/:id", hasUser(), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const recipe = await deleteById(id);
     res.json(recipe);
   } catch (error) {
     const message = mapErrors(error);
