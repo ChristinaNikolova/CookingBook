ae6e7927174a55db7949f6d25a6423415e7b04a5
christinanikolova <christinanikolova@github.com>
2025-12-08 13:50:28 +0200

    use recipe form in create recipe component

--- client/src/components/Recipe/Create/Create.jsx
+++ client/src/components/Recipe/Create/Create.jsx
@@ -2,10 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useForm from "../../../hooks/useForm";
 import useConfigToken from "../../../hooks/useConfigToken";
-import Button from "../../shared/Button/Button";
-import CustomInput from "../../shared/CustomInput/CustomInput";
-import CustomSelect from "../../shared/CustomSelect/CustomSelect";
-import ServerError from "../../shared/ServerError/ServerError";
+import FormRecipe from "../Form/Form";
 import { validator } from "../../../utils/helpers/validator";
 import requester from "../../../utils/helpers/requester";
 import { httpMethods, ids } from "../../../utils/constants/global";
@@ -146,121 +143,28 @@ export default function CreateRecipe() {
   };
 
   return (
-    <section id="create-recipe" className="section-form">
-      {serverError && <ServerError error={serverError} />}
-      <h2 ref={formRef} className="form-title">
-        Добави нова рецепта
-      </h2>
-      <form className="form" action={submitHandler}>
-        <CustomInput
-          label="Заглавие"
-          error={errors.title}
-          {...fieldHandler("title")}
-        />
-
-        <CustomInput
-          tag="textarea"
-          label="Резюме"
-          error={errors.summary}
-          {...fieldHandler("summary")}
-        />
-
-        <CustomInput
-          label="Необходимо време"
-          error={errors.neededTime}
-          {...fieldHandler("neededTime")}
-        />
-
-        <CustomInput
-          label="Брой порции"
-          type="number"
-          error={errors.portions}
-          {...fieldHandler("portions")}
-        />
-
-        <CustomSelect
-          label="Категория"
-          values={categories}
-          {...fieldHandler("category")}
-        />
-
-        <div className={styles["create-recipe-wrapper"]}>
-          <h4 className={styles["create-recipe-title"]}>Инструкции</h4>
-          {instructions.map((instruction, index) => (
-            <div key={index} className={styles["create-recipe-input-wrapper"]}>
-              <CustomInput
-                label={`Стъпка ${index + 1}`}
-                value={instruction}
-                onChange={(e) =>
-                  updateInstructionHandler(index, e.target.value)
-                }
-                onBlur={() => validateInstructionHandler(index)}
-                error={instructionErrors[index]}
-              />
-              <i
-                className="fas fa-times"
-                onClick={() => deleteInstructionHandler(index)}
-                title="Изтрий"
-              />
-            </div>
-          ))}
-          <Button
-            text=" + инструкция"
-            disabled={false}
-            onClick={() => addInputHandler("instruction")}
-          />
-        </div>
-
-        <div className={styles["create-recipe-wrapper"]}>
-          <h4 className={styles["create-recipe-title"]}>Продукти</h4>
-          {ingredients.map((ingredient, index) => (
-            <div key={index} className={styles["create-recipe-input-wrapper"]}>
-              <CustomInput
-                key={index}
-                label={`Продукт ${index + 1}`}
-                value={ingredient}
-                onChange={(e) => updateIngredientHandler(index, e.target.value)}
-                onBlur={() => validateIngredientHandler(index)}
-                error={ingredientErrors[index]}
-              />
-              <i
-                className="fas fa-times"
-                onClick={() => deleteIngredientHandler(index)}
-                title="Изтрий"
-              />
-            </div>
-          ))}
-          <Button
-            text="+ продукт"
-            disabled={false}
-            onClick={() => addInputHandler("ingredient")}
-          />
-        </div>
-
-        {currentImage && (
-          <CustomInput
-            label="Текущо изображение"
-            value={currentImage.name}
-            disabled
-          />
-        )}
-
-        <CustomInput
-          label="Изображение"
-          type="file"
-          error={errors.image}
-          {...fieldHandler("image")}
-        />
-
-        <CustomInput
-          label="Подходящо за бебе"
-          type="checkbox"
-          error={errors.isBabySafe}
-          {...fieldHandler("isBabySafe")}
-        />
-
-        <Button text="Създай рецепта" type="submit" disabled={!isFormValid()} />
-      </form>
-    </section>
+    <FormRecipe
+      type="create"
+      title="Създай рецепта"
+      categories={categories}
+      instructions={instructions}
+      ingredients={ingredients}
+      currentImage={currentImage}
+      serverError={serverError}
+      formRef={formRef}
+      errors={errors}
+      disabled={!isFormValid()}
+      submitHandler={submitHandler}
+      fieldHandler={fieldHandler}
+      instructionErrors={instructionErrors}
+      ingredientErrors={ingredientErrors}
+      addInputHandler={addInputHandler}
+      updateInstructionHandler={updateInstructionHandler}
+      validateInstructionHandler={validateInstructionHandler}
+      deleteInstructionHandler={deleteInstructionHandler}
+      updateIngredientHandler={updateIngredientHandler}
+      validateIngredientHandler={validateIngredientHandler}
+      deleteIngredientHandler={deleteIngredientHandler}
+    />
   );
 }
