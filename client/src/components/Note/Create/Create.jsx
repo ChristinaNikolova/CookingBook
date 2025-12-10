5047342257479e390bf0d5ad48ca9949e5ae7f96
christinanikolova <christinanikolova@github.com>
2025-12-10 07:01:58 +0200

    update create note to use useAction hook

--- client/src/components/Note/Create/Create.jsx
+++ client/src/components/Note/Create/Create.jsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import useForm from "../../../hooks/useForm";
-import useConfigToken from "../../../hooks/useConfigToken";
+import useAction from "../../../hooks/useAction";
 import Button from "../../shared/Button/Button";
 import CustomInput from "../../shared/CustomInput/CustomInput";
 import ServerError from "../../shared/ServerError/ServerError";
-import requester from "../../../utils/helpers/requester";
 import { httpMethods, serverPaths } from "../../../utils/constants/global";
 
 const initialValues = {
@@ -12,26 +11,19 @@ const initialValues = {
 };
 
 export default function CreateNote({ onCreate }) {
-  const [serverError, setServerError] = useState("");
-  const config = useConfigToken();
   const formRef = useRef();
 
+  const { execute, serverError } = useAction();
   const { fieldHandler, submitHandler, errors, disabledForm, setValues } =
     useForm(createHandler, "note", initialValues, formRef);
 
   async function createHandler(data) {
-    setServerError("");
     try {
-      const result = await requester(
-        serverPaths.NOTES,
-        httpMethods.POST,
-        data,
-        config
-      );
+      const result = await execute(serverPaths.NOTES, httpMethods.POST, data);
       setValues(initialValues);
       onCreate(result);
     } catch (err) {
-      setServerError(err.message);
+      console.error(err);
     }
   }
 
